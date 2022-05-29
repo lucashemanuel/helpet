eeae8923b7c86c27cd18d64f0dd95e16acc8fb29
lucashemanuel <lucashemanuel890@gmail.com>
2022-05-29 17:03:24 -0300

    new version

--- src/pages/CadastraPet/index.js
+++ src/pages/CadastraPet/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Botao, Container, Form, Header, HeaderImage, Input, Texto, Texto2 } from './style';
+import { Botao, Container, Form, Header, HeaderImage, Input, Texto, Texto2, Texto3, View } from './style';
 import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Keyboard, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback } from 'react-native';
+
+const schema = yup.object({
+  petnome:  yup.string().max(20, "Digite no máximo 20 letras!!!").required("Informe o nome do pet!!!"),
+  local:  yup.string().required("Informe onde o pet foi encontrado!!!"),
+  sexo: yup.string().required("Informe o sexo do pet!!!"),
+  raca:  yup.string().required("Informe a raça do pet!!!"),
+  idade:  yup.number().required("Informe a idade do pet!!!"),
+  peso: yup.string().max(5, "Peso inválido!!!").required("Informe o peso do pet!!!"),
+  porte: yup.string().max(15, "Porte inválido!!!").required("Informe o porte!!!")
+})
 
 export default function CadastraPet() {
-  const {control, handleSubmit, formState: { errors } } = useForm({})
+  const {control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   function handleSignIn(data) {
     console.log(data);
@@ -11,49 +26,66 @@ export default function CadastraPet() {
 
   return(
     <Container>
-        <Header>
-          <HeaderImage source={require('../../images/camera.png')}/>
-          <Texto2>Clique para tirar uma foto do Pet!</Texto2> 
-        </Header>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView>
 
-        <Form>
-          <Texto>Nome:</Texto>
-          <Controller control={control} name="pet-nome" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Nome do Pet"/>
-          )}/>
+            <Header>
+              <HeaderImage source={require('../../images/camera.png')}/>
+              <Texto2>Clique para tirar uma foto do Pet!</Texto2> 
+            </Header>
 
-          <Texto>Localização:</Texto>
-          <Controller control={control} name="localizacao" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Localização"/>
-          )}/>
+            <Form>
+              <Texto>Nome:</Texto>
+              <Controller control={control} name="petnome" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Nome do Pet"/>
+              )}/>
+              {errors.petnome && <Texto3>{errors.petnome?.message}</Texto3>}
 
-          <Texto>Sexo:</Texto>
-          <Controller control={control} name="sexo" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Sexo do Pet"/>
-          )}/>
+              <Texto>Localização:</Texto>
+              <Controller control={control} name="local" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Localização"/>
+              )}/>
+              {errors.local && <Texto3>{errors.local?.message}</Texto3>}
 
-          <Texto>Raça:</Texto>
-          <Controller control={control} name="raca" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Raça do Pet"/>
-          )}/>
+              <Texto>Sexo:</Texto>
+              <Controller control={control} name="sexo" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Sexo do Pet"/>
+              )}/>
+              {errors.sexo && <Texto3>{errors.sexo?.message}</Texto3>}
 
-          <Texto>Idade:</Texto>
-          <Controller control={control} name="idade" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Idade do Pet"/>
-          )}/>
+              <Texto>Raça:</Texto>
+              <Controller control={control} name="raca" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Raça do Pet"/>
+              )}/>
+              {errors.raca && <Texto3>{errors.raca?.message}</Texto3>}
 
-          <Texto>Peso:</Texto>
-          <Controller control={control} name="peso" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Peso do Pet"/>
-          )}/>
+              <Texto>Idade:</Texto>
+              <Controller control={control} name="idade" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite a Idade do Pet"/>
+              )}/>
+              {errors.idade && <Texto3>{errors.idade?.message}</Texto3>}
 
-          <Texto>Porte:</Texto>
-          <Controller control={control} name="porte" render={({ field: {onChange, onBlur, value} }) => (
-            <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Porte do Pet"/>
-          )}/>
+              <Texto>Peso (Kg):</Texto>
+              <Controller control={control} name="peso" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Peso do Pet"/>
+              )}/>
+              {errors.peso && <Texto3>{errors.peso?.message}</Texto3>}
 
-          <Botao activeOpacity={0.5} onPress={handleSubmit(handleSignIn)}><Texto2>Cadastrar Pet</Texto2></Botao>
-        </Form>
+              <Texto>Porte:</Texto>
+              <Controller control={control} name="porte" render={({ field: {onChange, onBlur, value} }) => (
+                <Input onChangeText={onChange} onBlur={onBlur} value={value} placeholder="Digite o Porte do Pet"/>
+              )}/>
+              {errors.porte && <Texto3>{errors.porte?.message}</Texto3>}
+              
+              <View>
+                <Botao activeOpacity={0.9} onPress={handleSubmit(handleSignIn)}><Texto2>Cadastrar Pet</Texto2></Botao>
+              </View>
+            </Form>
+
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </ScrollView>
     </Container>
   );
 }
--- src/pages/CadastraPet/style.js
+++ src/pages/CadastraPet/style.js
@@ -5,7 +5,8 @@ export const Container = styled.View`
 `;
 
 export const Header = styled.View`
- flex: 0.35;
+ width: 500px;
+ height: 250px;
  background-color: grey;
  align-items: center;
  justify-content: center;
@@ -14,11 +15,14 @@ export const Header = styled.View`
 export const HeaderImage = styled.Image`
   width: 30%;
   height: 50%;
+  margin-top: 45px;
   margin-bottom: 15px;
 `;
 
 export const Texto = styled.Text`
-
+  margin-bottom: 5px;
+  font-size: 17px;
+  font-weight: 700;
 `;
 
 export const Texto2 = styled.Text`
@@ -36,18 +40,27 @@ export const Botao = styled.TouchableOpacity`
 `;
 
 export const Input = styled.TextInput`
-  border: 1px solid #000000;
+  border: 1.5px solid #000;
   border-radius: 7px;
   justify-content: center;
   height: 35px;
   padding-left: 15px;
+  margin-bottom: 15px;
 `;
 
 
 export const Form = styled.View`
-  flex: 0.65;
+  margin-top: 10px;
   flex-direction: column;
-  justify-content: space-around;
-  padding: 20px;
+  padding: 25px;
 `;
 
+export const View = styled.View`
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const Texto3 = styled.Text`
+  color: #f37a7a;
+  margin-bottom: 10px;
+`;
