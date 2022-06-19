c89e7c81b31e9dc10f3f0ae78c7e30e2912fd5f3
lucashemanuel <lucashemanuel890@gmail.com>
2022-06-19 20:11:26 -0300

    update new screen

--- src/pages/FeedbackCadastro/index.js
+++ src/pages/FeedbackCadastro/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Container, Fundo } from "./style";
+import { Adesivo, Botao, Container, Fundo, Texto, Texto2, Texto3 } from "../FeedbackCadastro/style";
 
 export default function FeedbackCadastro() {
   return(
     <Container>
-      <Fundo/>
+      <Fundo source={require('../../images/bolas.png')} resizeMode="cover">
+
+        <Adesivo source={require('../../images/icon.png')}/>
+
+        <Texto>Parabéns!!</Texto>
+        <Texto2>Sua conta foi criada com sucesso.</Texto2>
+
+        <Botao><Texto3>Continuar</Texto3></Botao>
+      </Fundo>
     </Container>
   );
 }
